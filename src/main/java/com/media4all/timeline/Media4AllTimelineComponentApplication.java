ee6a0601208c3ff8ee4c15fb42bb09dd9edf8bbb
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2024-10-10 22:04:16 -0300

    chore: initial project setup

---
+++ src/main/java/com/media4all/timeline/Media4AllTimelineComponentApplication.java
@@ -0,0 +1,12 @@
+package com.media4all.timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Media4AllTimelineComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Media4AllTimelineComponentApplication.class, args);
+	}
+}
